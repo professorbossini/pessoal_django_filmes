7e09fc693b299af0ef34aba2c0b5367b7fd143b6
Rodrigo Bossini <professorbossini@gmail.com>
2023-09-24 18:32:46 -0300

    filmes: serializando generos

--- filmes_app/serializers.py
+++ filmes_app/serializers.py
@@ -1,6 +1,16 @@
 from rest_framework import serializers
 from .models import Filme
+from .models import Genero
+
+class GeneroSerializer (serializers.ModelSerializer):
+  class Meta:
+    model = Genero
+    fields = '__all__'
+
 class FilmeSerializer (serializers.ModelSerializer):
+  #o nome genero deve ser igual ao nome especificado no modelo de filme
+  genero = GeneroSerializer()
   class Meta:
     model = Filme
     fields = '__all__'
+
